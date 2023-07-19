d107d2b5c175730a59dc1bf642f2823bad018a96
192114 <shadowSun192114@gmail.com>
2023-07-19 17:55:30 +0800

    拖拽时 样式提示

--- apps/designer/src/components/TemplateList/TemplateList.tsx
+++ apps/designer/src/components/TemplateList/TemplateList.tsx
@@ -13,11 +13,11 @@ import 'simplebar-react/dist/simplebar.min.css'
 export function TemplateList() {
   const { templateList, activeType } = useTemplateListStore()
   const wrapperKey = useTemplateKeyStore((state) => state.wrapperKey)
-  const { active } = useDndContext()
+  const { active, over } = useDndContext()
 
   return (
     <SimpleBar
-      className={cn(styles.container, { [styles.active]: active })}
+      className={cn(styles.container, { [styles.active]: active && !over })}
       key={wrapperKey}
     >
       <p className={styles.alert}>拖拽添加模板</p>
